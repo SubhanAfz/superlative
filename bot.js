19ba47cadd48e422f062b9440486bb3beeefaa8b
roofingoof <subhanafzal125@gmail.com>
2019-10-08 20:39:33 +0100

    Added ban command
    Added ban command

--- bot.js
+++ bot.js
@@ -86,8 +86,34 @@ bot.on("message", message =>{
       message.channel.send({embed:msgembed});
 
     }
-    if (args[0] == "info"){
+    if (args[0] == "ban"){
+      var user = message.mentions.users.first();
+        
+        if (user) {
+            
+            const member = message.guild.member(user);
+            
+            if (member) {
+              
+              
+              member.ban(args[2]).then(() => {
+                
+                message.reply(`Successfully banned ${user.tag}`);
+              }).catch(err => {
+                
+                message.reply('I was unable to ban the member');
+                
+                console.error(err);
+              });
+            } else {
+              
+              message.reply('That user isn\'t in this guild!');
+            }
 
+          } else {
+            message.reply('You didn\'t mention the user to kick!');
+          }
+      
     }
     
       
