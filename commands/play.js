00d7f9215be714da9a35bdfdf6b32975610d5f02
roofingoof <subhanafzal125@gmail.com>
2019-10-12 21:42:22 +0100

    Fixed play command

--- commands/play.js
+++ commands/play.js
@@ -1,13 +1,20 @@
 const ytdl = require('ytdl-core');
-async function play(connection,message){
-    var server = servers[message.guild.id];
-    server.dispatcher = connection.playStream(server.queue[0], {filter: "audioonly"});
+function play(connection,message){
+    var server = servers[message.guild.id]
+    server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
     server.queue.shift();
     server.dispatcher.on("end", function(){
-        if (server.queue[0]) play(connection,message)
-        else connection.disconnect();
+        if (server.queue[0]){
+            play(connection,message);
+            
+        }
+        else{
+            connection.disconnect();
+        }
     });
+    
 }
+var servers ={}; 
 module.exports = class cchanneltext{
     constructor(){
         this.name = ".play",
@@ -16,8 +23,8 @@ module.exports = class cchanneltext{
     }
     async run(bot, message, args)
     {
-       var servers ={}; 
-  
+       
+        
         if(!args[1]){
             message.reply("You didn't give a link!")
             return;
@@ -26,18 +33,18 @@ module.exports = class cchanneltext{
             message.reply("You are not in a voice channel!")
             return;
         }
-        if(!args[1].startsWith("https://www.youtube.com"|| "https://youtu.be")){
+        if(!ytdl.validateURL(args[1])){
             message.reply("You can only provide youtube links!")
             return;
         }
-        if(!servers[message.guild.id])servers[message.guild.id] ={
-            queue: []
+        if(!servers[message.guild.id])
+        {
+            servers[message.guild.id]={queue:[]}
         }
-        var server = servers[message.guild.id];
-        server.queue.push(args[1])
-        
-        if(!message.guild.voiceConnection)message.member.voiceChannel.join().then(function(connection){
-            play(connection, message)
-        });
+        var server = servers[message.guild.id]
+        server.queue.push(queue);
+
+        play(connection, message)
+
     }
 }
