59e00a30220abf412ca7457700da759a89a0331a
roofingoof <subhanafzal125@gmail.com>
2019-10-21 21:42:22 +0100

    fixed backwards no space

--- commands/backwards.js
+++ commands/backwards.js
@@ -4,12 +4,13 @@ module.exports = class backwards{
         this.alias = ["back"],
         this.usage = ".backwards message"
     }
-    async run(bot, message, args,colours)
+    async run(bot, message,colours)
     {
+        var args = message.content.split(" ",2) // .backwards asdasd adsasdasd
         function reversestring(str){
             return str.split("").reverse().join("")
         }
-        var messageargs = args[1];
+        var messageargs = args[1]
         var reversedmsg = reversestring(messageargs)
         message.reply(`Your reversed string is ${reversedmsg}`)
     }
