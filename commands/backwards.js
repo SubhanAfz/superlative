9fdc31302233380ef2bfba065daa5dc536cb0820
roofingoof <subhanafzal125@gmail.com>
2019-10-21 22:32:54 +0100

    fix

--- commands/backwards.js
+++ commands/backwards.js
@@ -4,13 +4,12 @@ module.exports = class backwards{
         this.alias = ["back"],
         this.usage = ".backwards message"
     }
-    async run(bot, message,args,colours)
+    async run(bot, message, args,colours)
     {
-        
         function reversestring(str){
             return str.split("").reverse().join("")
         }
-        var messageargs = args[1]
+        var messageargs = args[1];
         var reversedmsg = reversestring(messageargs)
         message.reply(`Your reversed string is ${reversedmsg}`)
     }
