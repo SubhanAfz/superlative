6a2a36a8832cf52fbe002f006e21277722c211ec
roofingoof <subhanafzal125@gmail.com>
2019-10-21 21:57:14 +0100

    Update backwards.js

--- commands/backwards.js
+++ commands/backwards.js
@@ -4,13 +4,13 @@ module.exports = class backwards{
         this.alias = ["back"],
         this.usage = ".backwards message"
     }
-    async run(bot, message,colours)
+    async run(bot, message,args,colours)
     {
-        var args = message.content.split(" ",2) // .backwards asdasd adsasdasd
+        var args1 = message.content.split(" ",2) // .backwards asdasd adsasdasd
         function reversestring(str){
             return str.split("").reverse().join("")
         }
-        var messageargs = args[1]
+        var messageargs = args1[1]
         var reversedmsg = reversestring(messageargs)
         message.reply(`Your reversed string is ${reversedmsg}`)
     }
